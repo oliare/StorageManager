b98c27f16ae7dcca2d52ac95cbb4a54d71d3658a
Olia <oliaopanasiukk@gmail.com>
2024-03-16 23:24:17 +0200

    initial commit

---
+++ StorageManager/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+class StorageDb
+{
+    private SqlConnection connection;
+    public StorageDb(string connString)
+    {
+        connection = new SqlConnection(connString);
+        connection.Open();s
+        Console.WriteLine("true");
+    }
+}
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        StorageDb s = new StorageDb(ConfigurationManager.ConnectionStrings["StorageDb"].ConnectionString);
+    }
+}
